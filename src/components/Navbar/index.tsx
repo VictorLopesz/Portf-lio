5bf7c55a2ad303bcb60d1f64558c67f54a7dd970
Victor.Loperz <euvictor_h@hotmail.com>
2023-10-23 14:31:24 -0300

    navbar/sidebar concluída 90% (faltando só scroll suave)

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,24 +3,35 @@ import Link from 'next/link';
 import Image from 'next/image';
 import react, { useState } from 'react';
 import { AiFillGithub, AiFillLinkedin, AiOutlineMenu, AiOutlineClose, AiOutlineWhatsApp } from 'react-icons/ai';
+import {BiBookBookmark, BiSolidMessageRounded} from 'react-icons/bi';
+import {BsFillPersonVcardFill, BsPersonWorkspace} from 'react-icons/bs';
+import {GiHamburgerMenu, GiSkills} from 'react-icons/gi';
+import {CgCloseR} from 'react-icons/cg';
+import logotipo from '../../../public/assets/logotipo.png'
 
 
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false)
+    const [iconMenu, setIconMenu] = useState(false)
 
     const handleNav = () => {
         setMenuOpen(!menuOpen);
     }
 
+    const Nav = () => {
+        setIconMenu(!iconMenu);
+    }
+
+
     return (
-        <nav className="fixed w-full h-24 shadow-sm bg-white font-sansnarrow">
+        <nav className="fixed w-full h-24 shadow-sm bg-gradient-to-r from-[#0F182C] to-[#16304E] font-sansnarrow">
             <div className="flex justify-between items-center h-full px-4">
                 <Link href='/'>
                     <Image
-                        src={'/'}
+                        src={logotipo.src}
                         alt="logo"
-                        width="205"
+                        width="120"
                         height="75"
                         className="cursor-pointer"
                         priority
@@ -29,60 +40,61 @@ const Navbar = () => {
                 <div className="hidden sm:flex">
                     <ul className="hidden sm:flex text-sm mr-52 mt-8">
                         <Link href="/">
-                            <li className="pb-2 text-black uppercase py-2
-                            md:px-6 text-center text-bold
-                             hover:text-[#fff] hover:bg-[#58585880] active:bg-[#58585880]
-                             active:text-[#0f0d3ae1] 
+                            <li className=" text-white  p-2 uppercase py-2
+                            md:px-6 text-center 
+                             text-bold
+                             hover:text-[#8D6D3A] active:text-[#382b18]  hover:bg-[#58585880] active:bg-[#cacad26c]
+                             active:text-[#bebdc9e1] 
                              transition duration-500 ease-in-out">Sobre mim</li>
                         </Link>
                         <Link href="/">
-                            <li className="pb-2 text-black uppercase py-2
+                            <li className="pb-2 text-white  p-2 uppercase py-2
                             md:px-6 text-center 
                              text-bold
-                             hover:text-[#fff] hover:bg-[#58585880] active:bg-[#58585880]
-                             active:text-[#0f0d3ae1] 
+                             hover:text-[#8D6D3A] active:text-[#382b18]  hover:bg-[#58585880] active:bg-[#cacad26c]
+                             active:text-[#bebdc9e1] 
                              transition duration-500 ease-in-out">Skills</li>
                         </Link>
                         <Link href="/">
-                            <li className="pb-2 text-black uppercase py-2
+                            <li className="pb-2 text-white  p-2 uppercase py-2
                             md:px-6 text-center 
                              text-bold
-                             hover:text-[#fff] hover:bg-[#58585880] active:bg-[#58585880]
-                             active:text-[#0f0d3ae1] 
-                             transition duration-500 ease-in-out ">Portfólio</li>
+                             hover:text-[#8D6D3A] active:text-[#382b18]  hover:bg-[#58585880] active:bg-[#cacad26c]
+                             active:text-[#bebdc9e1] 
+                             transition duration-500 ease-in-out">Portfólio</li>
                         </Link>
                         <Link href="/">
-                            <li className="pb-2 text-black uppercase py-2
+                            <li className="pb-2 text-white  p-2 uppercase py-2
                             md:px-6 text-center 
                              text-bold
-                             hover:text-[#fff] hover:bg-[#58585880] active:bg-[#58585880]
-                             active:text-[#0f0d3ae1]  
-                             transition duration-500 ease-in-out ">Cursos</li>
+                             hover:text-[#8D6D3A] active:text-[#382b18]  hover:bg-[#58585880] active:bg-[#cacad26c]
+                             active:text-[#bebdc9e1] 
+                             transition duration-500 ease-in-out">Cursos</li>
                         </Link>
                         <Link href="/">
-                            <li className="pb-2 text-black uppercase py-2
+                            <li className="pb-2 text-white  p-2 uppercase py-2
                             md:px-6 text-center 
                              text-bold
-                             hover:text-[#fff] hover:bg-[#58585880] active:bg-[#58585880]
-                             active:text-[#0f0d3ae1]  
-                             transition duration-500 ease-in-out ">Contato</li>
+                             hover:text-[#8D6D3A] active:text-[#382b18]  hover:bg-[#58585880] active:bg-[#cacad26c]
+                             active:text-[#bebdc9e1] 
+                             transition duration-500 ease-in-out">Contato</li>
                         </Link>
                     </ul>
                 </div>
-                <div onClick={handleNav} className="md:hidden cursor-pointer pl-24">
-                    <AiOutlineMenu size={25} className=" transition duration-300 ease-in-out text-[#38BDF8] hover:text-[#2c4f5e]" />
+                <div onClick={handleNav} className="md:hidden cursor-pointer">
+                    <GiHamburgerMenu size={25} className=" transition duration-300 ease-in-out text-[#8D6D3A] active:text-[#382b18]" />
                 </div>
             </div>
             <div className={
                 menuOpen
-                    ? "fixed left-0 overflow-hidden top-0 w-[45%] sm:hidden h-screen bg-[#2c4f5ee4] p-10 ease-in transition duration-500" :
-                    "fixed left-[100%] top-0 p-10 transition ease-out translate-x-1 overflow-hidden duration-500 "
+                    ? "fixed left-0 top-0 w-[45%] sm:hidden h-screen bg-gradient-to-r from-[#0F182C] to-[#16304E] p-10 ease-in overflow-x-auto overflow-y-hidden transition duration-75" :
+                    "fixed left-[100%] top-0 p-10 transition ease-out -translate-x-4 duration-75 overflow-y-auto overflow-x-hidden "
             }
             >
 
                 <div className="flex w-full items-center justify-end">
-                    <div onClick={handleNav} className="cursor-pointer  text-[#38BDF8] hover:text-[#000]">
-                        <AiOutlineClose size={25} />
+                    <div onClick={handleNav} className="cursor-pointer text-[#8D6D3A] active:text-[#382b18]">
+                        <CgCloseR size={25} />
                     </div>
                 </div>
                 <br />
@@ -91,37 +103,37 @@ const Navbar = () => {
                     <ul>
                         <Link href="/">
                             <li onClick={() => setMenuOpen(false)}
-                                className="py-2 transition duration-300 ease-in-out cursor-pointer hover:text-[#b3b9bb] active:text-[#0f0d3ae1]"
+                                className="py-1 flex items-center transition  duration-300 ease-in-out cursor-pointer active:text-[#8D6D3A]"
                             >
-                                Sobre mim
+                               <BsFillPersonVcardFill className="m-2"/> Sobre mim
                             </li>
                         </Link>
                         <Link href="/">
                             <li onClick={() => setMenuOpen(false)}
-                                className="py-2 transition duration-300 ease-in-out cursor-pointer hover:text-[#b3b9bb] active:text-[#0f0d3ae1]"
+                                className="py-1 flex items-center transition duration-300 ease-in-out cursor-pointer  active:text-[#8D6D3A]"
                             >
-                                Skills
+                                 <GiSkills className="m-2"/>  Skills
                             </li>
                         </Link>
                         <Link href="/">
                             <li onClick={() => setMenuOpen(false)}
-                                className="py-2 transition duration-300 ease-in-out cursor-pointer hover:text-[#b3b9bb] active:text-[#0f0d3ae1]"
+                                className="py-1 flex items-center transition duration-300 ease-in-out cursor-pointer  active:text-[#8D6D3A]"
                             >
-                                Portfólio
+                               <BsPersonWorkspace className="m-2"/> Portfólio
                             </li>
                         </Link>
                         <Link href="/">
                             <li onClick={() => setMenuOpen(false)}
-                                className="py-2 transition duration-300 ease-in-out cursor-pointer hover:text-[#b3b9bb] active:text-[#0f0d3ae1]"
+                                className="py-1 flex items-center transition duration-300 ease-in-out cursor-pointer active:text-[#8D6D3A]"
                             >
-                                Cursos
+                               <BiBookBookmark className="m-2"/> Cursos
                             </li>
                         </Link>
                         <Link href="/">
                             <li onClick={() => setMenuOpen(false)}
-                                className="py-2 transition duration-300 ease-in-out cursor-pointer hover:text-[#b3b9bb] active:text-[#0f0d3ae1]"
+                                className="py-1 flex items-center transition duration-300 ease-in-out cursor-pointer hover:text-[#8d6096] active:text-[#8D6D3A]"
                             >
-                                Contato
+                               <BiSolidMessageRounded className="m-2"/> Contato
                             </li>
                         </Link>
 
@@ -132,15 +144,15 @@ const Navbar = () => {
 
                 <div className="flex items-center text-white mt-5 text-2xl">
                     <Link href={'/'}>
-                        <AiFillGithub className="m-2 hover:invert transition duration-200 ease-in-out active:text-[#0f0d3ae1]"/>
+                        <AiFillGithub className="m-2 active:text-[#000000] transition duration-200 ease-in-out"/>
                     </Link>
 
                     <Link href={'/'}>
-                        <AiFillLinkedin className="m-2 hover:invert transition duration-200 ease-in-out active:text-[#0f0d3ae1]"/>
+                        <AiFillLinkedin className="m-2 active:text-[#0A66C2] transition duration-200 ease-in-out "/>
                     </Link>
 
                     <Link href={'/'}>
-                        <AiOutlineWhatsApp className="m-2 hover:invert transition duration-200 ease-in-out active:text-[#0f0d3ae1]"/>
+                        <AiOutlineWhatsApp className="m-2 active:text-[#25D366] transition duration-200 ease-in-out"/>
                     </Link>
                 </div>
                 </div>
