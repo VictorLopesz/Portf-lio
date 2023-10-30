e6e06639e08118554c0857f6551508c928bc326e
VictorLopesz <euvictor_h@hotmail.com>
2023-10-30 18:00:48 -0300

    algumas mudanças

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -6,22 +6,29 @@ import React, { useState, useEffect } from 'react';
 export default function Skills() {
   const [mySkills, setMySkills] = useState([]);
 
-  
+
 
 
   return (
     <>
+      <div>
+      <h1 
+      className="uppercase font-sansarrow
+      flex items-center font-bold justify-center
+      text-4xl text-[#e7d5bd]">
+        skills
+      </h1>
+      </div>
     <div className="flex items-center justify-center">
     <div className="
-    bg-[#D2B48C] w-[80%] h-[80%] rounded-sm 
-    backdrop-blur-sm shadow-xl
+    bg-[#BB9E70] w-[80%] h-[80%] rounded-sm 
+    backdrop-blur-sm shadow-xl flex items-center justify-center
     
     ">
-      <h1 className="uppercase font-sansarrow flex items-center justify-center">skills</h1>
       <div 
       className="
-      flex items-center justify-evenly
-       text-white font-sansnarrow
+      flex items-center justify-between space-x-10
+       text-black font-sansnarrow
        ">
           <span>HTML / CSS</span>
 
