641186c489218c1ab2f14392479c30cb70ab2cd1
Victor.Loperz <euvictor_h@hotmail.com>
2023-10-31 08:26:19 -0300

    começando skills de novo

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,19 +1,75 @@
 'use client'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useTransition } from 'react';
+import TabButton from '../TabButton';
 
 
 
 export default function Skills() {
-  const [mySkills, setMySkills] = useState([]);
 
+  const [tab, setTab] = useState('skills');
+  const [isPending, startTransition] = useTransition();
 
-
+  const handleTabChange = (id: any) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  }
 
   return (
     <>
-      <div>
+      <div className="flex items-center justify-center text-white">
+        <div className="flex flex-row mt-8 uppercase font-sansnarrow">
+          <div>
+            <span className="">
+              <TabButton setTab={() => handleTabChange('HTMLCSS')} active={tab === 'HTMLCSS'}>
+                HTML/CSS {" "}
+              </TabButton>
+            </span>
+          </div>
 
-    </div>
+          <div>
+            <span className="">
+              <TabButton setTab={() => handleTabChange('Tailwindcss')} active={tab === 'Tailwindcss'}>
+                Tailwind Css {" "}
+              </TabButton>
+            </span>
+          </div>
+
+          <div>
+            <span className="">
+              <TabButton setTab={() => handleTabChange('reactjs')} active={tab === 'reactjs'}>
+                React.Js {" "}
+              </TabButton>
+            </span>
+          </div>
+
+          <div>
+            <span className="">
+              <TabButton setTab={() => handleTabChange('nextjs')} active={tab === 'nextjs'}>
+                Next.Js {" "}
+              </TabButton>
+            </span>
+          </div>
+
+
+          <div>
+            <span className="">
+              <TabButton setTab={() => handleTabChange('php')} active={tab === 'php'}>
+                PHP {" "}
+              </TabButton>
+            </span>
+          </div>
+
+          <div>
+            <span className="">
+              <TabButton setTab={() => handleTabChange('sql')} active={tab === 'sql'}>
+                SQL {" "}
+              </TabButton>
+            </span>
+          </div>
+
+        </div>
+      </div>
     </>
   )
 };
--- src/components/TabButton/index.tsx
+++ src/components/TabButton/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const TabButton = ({ active, selectTab, children }: any) => {
-    const ButtonClasses = active ? 'text-[#fff] border-b-2 border-[#8D6D3A] text-[#a58148]' : ''
+    const ButtonClasses = active ? 'text-white border-b border-[#8D6D3A]' : 'text-[#a58148]'
 
     return (
         <button onClick={selectTab}>
-            <p className={`mr-3 text-white ${ButtonClasses}`}>
+            <p className={`mr-3 text-white hover:text-white ${ButtonClasses}`}>
                 {children}
             </p>
         </button>
