bf824a460201081a6db9d9f5706455b39846364b
Victor.Loperz <euvictor_h@hotmail.com>
2023-12-20 15:01:44 -0300

    side bar finalizada

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,20 @@
 'use client'
 import React, { useState } from 'react';
 import { Link } from "react-scroll";
-import { BsFillPersonVcardFill, BsPersonWorkspace } from 'react-icons/bs';
+import { BsPersonWorkspace } from 'react-icons/bs';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { CgCloseR } from 'react-icons/cg';
 import { IoIosHome } from "react-icons/io";
-import { MdEmail } from 'react-icons/md';
-import { IoPersonSharp } from "react-icons/io5";
+import { IoHomeOutline, IoMailOutline, IoPersonOutline, IoPersonSharp } from "react-icons/io5";
 import { BiSolidContact } from "react-icons/bi";
-import { Dropdown, DropdownTrigger, DropdownMenu, Button, DropdownItem } from "@nextui-org/react";
-import { FaGithub, FaInstagram, FaLinkedin, FaTelegramPlane, FaWhatsapp } from 'react-icons/fa';
+import { FaCode, FaGithub, FaInstagram, FaLinkedinIn, FaRegFileCode, FaTelegram, FaTelegramPlane, FaWhatsapp, } from 'react-icons/fa';
+import { TiContacts } from 'react-icons/ti';
+import { PiTelegramLogoDuotone } from 'react-icons/pi';
 
 
 const Navbar = ({ isScrolled }: any) => {
     const [menuOpen, setMenuOpen] = useState(false);
     const [activeLink, setActiveLink] = useState("inicio");
-
 
 
     const handleNav = () => {
@@ -26,7 +25,6 @@ const Navbar = ({ isScrolled }: any) => {
         setActiveLink(link);
         setMenuOpen(false);
     };
-
 
     return (
         <div id="nav" className={` ${isScrolled ? 'bg-[#000000] text-white text-3xl fixed z-50 transition duration-150 ease-in'
@@ -99,8 +97,7 @@ const Navbar = ({ isScrolled }: any) => {
                             duration={500}
                             spy={false}
                             offset={-80}
-                            activeClass="active"
-
+                            activeClass="link-ativo"
                         >
 
                             <li
@@ -122,35 +119,39 @@ const Navbar = ({ isScrolled }: any) => {
             </div>
             <div className={
                 menuOpen
-                    ? "fixed left-0 top-0 w-[55%] md:z-50 sm:hidden h-screen text-white bg-[#000000] p-4 ease-in overflow-x-auto overflow-y-hidden transition duration-75" :
-                    "fixed left-[100%] top-0 p-4 transition ease-out -translate-x-6 duration-200 overflow-y-auto overflow-x-hidden"
+                    ? "fixed left-0 top-0 w-[93%] md:z-50 p-3 rounded-r-lg sm:hidden h-screen transform -translate-x-0 text-white bg-[#000000] ease-in-out overflow-x-auto overflow-y-hidden transition duration-150" :
+                    "fixed left-[100%] top-0 p-4 rounded-r-lg ease-in-out transition -translate-x-6 duration-200 overflow-y-hidden overflow-x-auto"
             }
             >
-                <div className="flex w-full items-center justify-end">
-                    <div onClick={handleNav} className="cursor-pointer text-[#a09f9f] active:text-[#c35555]">
+                <div className="flex pr-2 w-full items-center justify-end">
+                    <div onClick={handleNav} className="cursor-pointer text-[#c1bfbf] pt-2 active:text-[#c35555]">
                         <CgCloseR size={25} />
                     </div>
                 </div>
                 <br />
                 <br />
-                <br />
-                {/* <Image
-                    src={logotipo}
-                    alt="logot"
-                    className="cursor-pointer" /> */}
-                <div className="flex-col text-white text-sm">
+                <h1 className="w-full flex text-[#aab6b9] -mt-16 justify-center text-xl items-center font-semibold">
+                    <FaCode className="mr-2 text-[#33b0cc]" />
+                    Portfólio
+                </h1>
+                <div className="flex-col mt-9 text-white text-xs pl-5">
                     <ul>
                         <Link
                             to="inicio"
                             smooth={true}
                             duration={500}
-                            spy={false}
+                            spy={true}
                             offset={-120}
+                            activeClass="link-ativo"
+
                         >
                             <li onClick={() => setMenuOpen(false)}
-                                className="py-2 flex items-center transition duration-300 font-sansnarrow text-base ease-in-out cursor-pointer active:text-[#535353]"
+                                className="mt-4 mb-3 flex items-center text-[#cfcccc] transition duration-300 justify-start  text-[14px] ease-in-out cursor-pointer active:text-[#535353]"
                             >
-                                <IoIosHome className="mr-3 w-[20px] h-[20px]" /> Início
+                                <IoHomeOutline className=" -ml-6 text-[#88c2df] mr-4 w-[20px] h-[20px]" />
+                                <span className="mt-1">
+                                    Início
+                                </span>
                             </li>
                         </Link>
                         <Link
@@ -159,11 +160,17 @@ const Navbar = ({ isScrolled }: any) => {
                             duration={500}
                             spy={true}
                             offset={-60}
+                            activeClass="link-ativo"
+
                         >
                             <li onClick={() => setMenuOpen(false)}
-                                className="py-2 flex items-center transition  duration-300 font-sansnarrow text-base ease-in-out cursor-pointer active:text-[#535353]"
+                                className="mt-2 mb-3  flex items-center text-[#cfcccc] transition justify-start duration-300 text-[14px] ease-in-out cursor-pointer active:text-[#535353]"
                             >
-                                <BsFillPersonVcardFill className="mr-3 w-[20px] h-[20px]" /> Sobre Mim
+                                <IoPersonOutline className=" -ml-6 text-[#88c2df] mr-4 w-[20px] h-[20px]" />
+                                <span 
+                                className="mt-1">
+                                    Sobre Mim
+                                </span>
                             </li>
                         </Link>
                         <Link
@@ -172,13 +179,15 @@ const Navbar = ({ isScrolled }: any) => {
                             duration={500}
                             spy={true}
                             offset={-80}
-                            activeClass="active"
-
+                            activeClass="link-ativo"
                         >
                             <li onClick={() => setMenuOpen(false)}
-                                className="py-2 flex items-center transition duration-300 font-sansnarrow text-base ease-in-out cursor-pointer active:text-[#535353]"
+                                className="mt-2 mb-3  flex items-center text-[#cfcccc] transition duration-300 justify-start  text-[14px] ease-in-out cursor-pointer active:text-[#535353]"
                             >
-                                <BsPersonWorkspace className="mr-3 w-[20px] h-[20px]" /> Projetos
+                                <FaRegFileCode className="-ml-6 text-[#88c2df] mr-4 w-[20px] h-[20px]" />
+                                <span className="mt-1">
+                                    Projetos
+                                </span>
                             </li>
                         </Link>
                         <Link
@@ -187,34 +196,55 @@ const Navbar = ({ isScrolled }: any) => {
                             duration={500}
                             spy={true}
                             offset={-80}
-                            activeClass="active"
+                            activeClass="link-ativo"
 
                         >
 
                             <li onClick={() => setMenuOpen(false)}
-                                className="py-2 flex items-center transition duration-300 font-sansnarrow text-base ease-in-out cursor-pointer active:text-[#535353]"
+                                className="mt-2 mb-3  flex items-center text-[#cfcccc] transition duration-300 justify-start  text-[14px] ease-in-out cursor-pointer active:text-[#535353]"
                             >
-                                <BiSolidContact className="mr-3 w-[20px] h-[20px]" /> Contato
+                                <TiContacts className="-ml-[26px] text-[#88c2df] mr-3 w-[22px] h-[22px]" />
+                                <span className="mt-1 ml-0.5">
+                                    Contato
+                                </span>
                             </li>
                         </Link>
                     </ul>
                 </div>
-                <div className="flex justify-center items-center w-full">
-                    <Dropdown>
-                        <DropdownTrigger>
-                            <Button
-                                variant="bordered"
-                            >
-                                <span className="text-white uppercase">Mídias Sociais</span>
-                            </Button>
-                        </DropdownTrigger>
-                        <DropdownMenu>
-                            <DropdownItem className="flex items-center justify-center"><MdEmail /><FaInstagram /><FaLinkedin /></DropdownItem>
-                            <DropdownItem className="flex items-center justify-center"><FaTelegramPlane /><FaWhatsapp /><FaGithub /></DropdownItem>
-                        </DropdownMenu>
-                    </Dropdown>
+                <hr />
+                <span className="text-[#939393] ml-6 text-[12px] font-bold uppercase -pl-2">contato</span>
 
+                <div className="grid grid-cols-1 ml-6">
+                    <div className="w-full flex items-center">
+                        <IoMailOutline className="w-[20px] h-[20px] mr-4 ml-2 text-[#88c2df]" />
+                        <span className="text-[#939393] text-[13px]">euvictor_h@hotmail.com</span>
+                    </div>
+                    <div className="w-full flex items-center">
+                        <FaWhatsapp className="w-[20px] h-[20px] mr-4 ml-2 text-[#88c2df]" />
+                        <span className="text-[#939393] text-[13px]">+55 21 9 8026-0633</span>
+                    </div>
+                    <div className="w-full flex items-center">
+                        <PiTelegramLogoDuotone className="w-[20px] h-[20px] mr-4 ml-2 text-[#88c2df]" />
+                        <span className="text-[#939393] text-[13px]">@pleasevictor</span>
+                    </div>
                 </div>
+                <hr />
+                <span className="ml-6 text-[#939393] text-[12px] font-bold uppercase -pl-2">mídias sociais</span>
+
+                <div className=" flex items-center justify-center mt-2 gap-x-2.5">
+                    <a href="https://github.com/VictorLopesz" className="no-underline text-current">
+                        <FaGithub className="w-6 h-6 mt-1 text-[#8899de]" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/lopesvictorh/" className="no-underline text-current">
+                        <FaLinkedinIn className="w-6 h-6 mt-1 text-[#58baeb]"/>
+                    </a>
+                    <a href="https://www.instagram.com/_pleasevictor/" className="no-underline text-current">
+                        <FaInstagram className="w-6 h-6 mt-1 text-[#c17e40]" />
+                    </a>
+                </div>
+                <span className="text-[#939393] mt-6 ml-4 text-[13px] flex items-center justify-center">
+                    Desenvolvido por Victor Lopes
+                </span>
             </div>
         </div >
     );
