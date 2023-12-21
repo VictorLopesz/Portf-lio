5e521c451b3ace6eaf19f7cab04f5d39ebc3e988
Victor.Loperz <euvictor_h@hotmail.com>
2023-12-21 14:41:24 -0300

    corrige responsividade

--- src/components/Projetos/index.tsx
+++ src/components/Projetos/index.tsx
@@ -8,7 +8,7 @@ const Projetos = () => {
 
       <div className="mt-2 h-screen">
       <h2 className="text-[#5e91b6] ml-3 mb-3 sm:mt-3 font-lato font-bold text-2xl mt-3">Projetos</h2>
-        <SlideShow />
+        <SlideShow/>
       </div>
     </div>
   )
