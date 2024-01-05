4ecc88c0a100532fa106cbc8bb5f4a02a0a534ff
VictorLopesz <131501443+VictorLopesz@users.noreply.github.com>
2024-01-05 07:27:14 -0300

    Atualizar o index.tsx

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -21,7 +21,7 @@ const Welcome = () => {
                 <div className="grid grid-cols-1 lg:grid-cols-12 ">
                     <div className="place-self-center col-span-7 text-center sm:text-left">
                         <h1 className="text-white mb-4 text-4xl md:text-4xl lg:text-4xl font-extrabold">
-                            <span className="text-transparent text-5xl md:mt-10 bg-clip-text bg-[#2b6b98]">
+                            <span className="text-transparent text-4xl md:mt-10 bg-clip-text bg-[#2b6b98]">
                                 Olá, eu sou
                                 <p>
                                     {" "}
