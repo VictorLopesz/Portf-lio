07e1b7d7a59b27d46e9244cd3a474aca29dafa04
Victor Lopes <euvictor_h@hotmail.com>
2024-08-03 21:43:50 -0300

    adicionando novo currículo

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -65,7 +65,7 @@ const Welcome = () => {
                                 <button
                                     className="bg-[#0254ab85] hover:bg-[#2b466394] active:bg-[#5a8dc385] text-[#e2e2e3] hover:text-[#c0c0c0] active:text-[#777777] transition duration-150 items-center flex justify-center px-4 py-2 font-semibold text-sm shadow-2xl rounded-md">
                                     <a
-                                        href="https://drive.google.com/file/d/1m3O3ztzfyIoYMKSX-RL7YPC99h0Cr9bX/view?usp=sharing"
+                                        href="https://drive.google.com/file/d/1H8GrKvmlh2ZrKkG9Q5bKD4gyfqi6IQ6V/view?usp=sharing"
 
                                         className="no-underline text-current flex justify-evenly items-center mt-2"
                                         target="_blank"
